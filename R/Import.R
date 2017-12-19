1ba15ea86cd6f7958fc2a1442756e2938c22cc30
Roland Kaiser <kardinal.eros@gmail.com>
2017-12-19 12:51:31 +0100

    added additional data set, cleaned man files

--- R/Import.R
+++ R/Import.R
@@ -1,6 +1,8 @@
 #	script to import and save as rda file
 if (FALSE) {
 
+#	BEV
+
 BEV <- readOGR("/Users/roli/Dropbox/ENNACON/resourcen/shp/austria_bundle/pg_administrative_boundaries", "pg_administrative_boundaries", p4s = "+init=epsg:31287", stringsAsFactors = FALSE)
 
 BEV <- spTransform(BEV, CRS("+init=epsg:4326"))
@@ -15,6 +17,11 @@ Encoding(BEV$BL) <- "UTF-8"
 Encoding(BEV$ST) <- "UTF-8"
 
 save(BEV, file = "/Users/roli/Documents/sabotag-data/data/BEV.rda", compress = "xz")
+
+#	Austria
+Austria <- readOGR("/Users/roli/Dropbox/sabotag/dta/shp/bundles/austria/pl_aut_neighbours_frame", "pl_aut_neighbours_frame", p4s = "+init=epsg:4326", stringsAsFactors = FALSE)
+
+save(Austria, file = "/Users/roli/Documents/sabotag-data/data/Austria.rda")
 
 relief <- brick("~/Dropbox/sabotag/dta/tif/bundle/austria/relief aut.tif")
 relief <- readAll(relief)
--- R/ecrins.R
+++ R/ecrins.R
@@ -1,7 +1,7 @@
 #	utility function to loadf both datasets
 ecrins <- 
 function () {
-	data(rivers, package = "sabotagdata")
-	data(lakes, package = "sabotagdata")
+	data("rivers", package = "sabotagdata")
+	data("lakes", package = "sabotagdata")
 	message("loaded rivers and lakes data sets")
 }
